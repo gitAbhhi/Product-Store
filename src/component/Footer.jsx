732760a5fd78686ddb14be27990b4dd2abce08c1
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2025-04-12 20:01:37 +0530

    footer complete

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Content, Footer, Header } from 'antd/es/layout/layout'
 const Footerpage = () => {
     return (
-        <Footer className='bg-gray-800'>
+        <Footer className='bg-gray-800 '>
             <h1 className='text-2xl'>Footer</h1>
 
         </Footer >
